7eeee7fbd1853f14f512c8e38705c696b66f8681
Kirk Santos <kirkalyn13@gmail.com>
2025-02-15 21:49:38 +0800

    initial commit

---
+++ src/test/java/com/engrkirky/mgnlfeaturesaidocs/MgnlfeaturesaidocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engrkirky.mgnlfeaturesaidocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgnlfeaturesaidocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
